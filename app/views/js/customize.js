4add7d4f3a765b46629266a64b02acb6b098f0f3
ibneurona <neurona007@gmail.com>
2019-07-08 17:21:04 -0500

    Cambio de colores en CSS

--- app/views/js/customize.js
+++ app/views/js/customize.js
@@ -1,10 +1,14 @@
 $(function(){
+  
   $(window).scroll(function(){
     if ($(document).scrollTop() > 50){
       $('nav').addClass('navshrink');
+      $('.navbar-nav .nav-item li').css('font-color','#ffffff');
     }
     else {
       $('nav').removeClass('navshrink');
+      $('.navbar-nav .nav-item li').css('font-color', '#ffffff')
+
     }
   })
 })
