ffbad1e81e7413aeb75574540b6a427176376cfb
ibneurona <neurona007@gmail.com>
2019-07-18 21:53:40 -0500

    js

--- app/views/js/customize.js
+++ app/views/js/customize.js
@@ -42,17 +42,6 @@ $(function () {
       
   })
 
-  // $('#blogCarousel').carousel({
-  //   interval: 5000
-  // });
-
-  // $('#news-slider').owlCarousel({
-  //   items: 2,
-  //   itemsDesktop: [1199, 2],
-  //   itemsMobile: [600, 1],
-  //   pagination: true,
-  //   autoPlay: true
-  // });
 })
 
 
